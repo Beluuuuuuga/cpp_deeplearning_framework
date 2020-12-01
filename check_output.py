4520f1e411c8c945adf863dc8f8c1a7283e0c550
井上 顧基 <inoue.koki15@gmail.com>
2020-12-01 18:48:52 +0900

    check weight

--- check_output.py
+++ check_output.py
@@ -13,18 +13,41 @@ def get_output(model, layer_name, sample):
 def convert_dense_output(output):
     return np.transpose(output, axes=[3,1,2,0])
 
+def convert_conv(w):
+    w = np.transpose(w, axes=[3,2,0,1])
+    return w
+
+
 if __name__ == "__main__":
 
     model = load_model("models/base_model.hdf5")
     # Numpy load
     loaded_sample = np.load('data/sample.npy')
     
-    print(loaded_sample.shape)
+    print("sample image shape:",loaded_sample.shape)
     print(model.predict(loaded_sample))
 
-    output = get_output(model, 'conv2', loaded_sample)
-    convert_conv2_output = convert_dense_output(output).ravel()
+    # 出力
+    conv2_output = get_output(model, 'conv2', loaded_sample)
+    convert_conv2_output = convert_dense_output(conv2_output).ravel()
+
+    pool1_output = get_output(model, 'maxpool1', loaded_sample)
+    print("maxpool1 output shape:",pool1_output.shape)
+
+    flatten_output = get_output(model, 'flatten1', loaded_sample)
+    print("flatten output shape:",flatten_output.shape)
+
+    dense1_output = get_output(model, 'dense1', loaded_sample)
+    print("dense1 output shape:",dense1_output.shape)
+
+    # 重み
+    dense1_weight = np.array(model.get_weights()[4])
+    print("dense1 weight shape:",dense1_weight.shape)
+
+
+
+
     # print(convert_conv2_output[:20])
-    for i in range(40):
-        print(convert_conv2_output[i])
+    # for i in range(40):
+    #     print(convert_conv2_output[i])
     
